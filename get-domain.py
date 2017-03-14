4dbbe571964224e4702bbc0782dcc31cbf21171d
ProbieK <joshuackroger@gmail.com>
2017-03-14 09:39:13 -0400

    Updated to make output OpenDNS compatible

--- get-domain.py
+++ get-domain.py
@@ -2,5 +2,5 @@ from urllib.parse import urlsplit
 import sys
 
 url = sys.argv[1]
-base_url = "{0.scheme}://{0.netloc}/".format(urlsplit(url))
+base_url = "{0.netloc}".format(urlsplit(url))
 print(base_url)
